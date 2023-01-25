71083c9eb0403d108bf05b853313cf49f904c760
seohyeon <fltk0825@naver.com>
2023-01-26 00:05:04 +0900

    타입을 미리 정하기 애매할 때 (union type, any, unknown)

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-var 이름 = "kim";
-var 나이 = 40;
-var 결혼했니 = undefined; //undefined이나 null 타입도 있다.
-var 회원들 = ["kim", "park"]; //array안에 들어올 요소의 타입을 []앞에 써준다.
-var 멤버들 = {
-    member1: "kim",
-    member2: "seo",
+//Union Type(타입2개 이상 합친 새로운 타입만들기)
+var 회원 = 123;
+회원; //할당해주면 type이 number로 뜸
+var 회원들 = [1, "2", 3];
+var 오브젝트 = { a: 123 };
+//any Type - 모든 자료형 허용해줌 => 타입실드 해제문법임. 타입스크립트 쓰는 의미가 없음. 기능 잃음.
+//그래서 타입관련 버그가 나도 잡아주지 않음
+var 이름;
+이름 = 123;
+이름 = [];
+//unknown 타입 - 모든 자료형 허용해줌. 최신문법. any문제해결
+var 네임;
+네임 = 123;
+네임 = {};
+//네임 - 1; // unknown타입은 number타입이 아니라 수학연산이 안됨. 같은 타입끼리의 연산만 가능함. 엄격함.
+//let 변수1: string = 네임; // unknown시에는 오류남, any는 오류안남
+var 나이;
+//나이 + 1; //안됨. 유니언타입에서는 안됨. 둘중 하나의 타입이었으면 +연산가능함.
+//문제1
+var user = "kim";
+var age = undefined;
+var married = false;
+var 철수 = [user, age, married];
+//문제2
+var 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
 };
-var project = {
-    member: ["kim", "park"],
-    days: 30,
-    started: true,
-};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,19 +1,42 @@
-let 이름 = "kim";
-let 나이: number = 40;
-let 결혼했니: undefined = undefined; //undefined이나 null 타입도 있다.
-let 회원들: string[] = ["kim", "park"]; //array안에 들어올 요소의 타입을 []앞에 써준다.
+//Union Type(타입2개 이상 합친 새로운 타입만들기)
+let 회원: number | string = 123;
+회원; //할당해주면 type이 number로 뜸
 
-let 멤버들: { member1: string; member2: string } = {
-  member1: "kim",
-  member2: "seo",
-};
+let 회원들: (number | string)[] = [1, "2", 3];
+let 오브젝트: { a: string | number } = { a: 123 };
 
-let project: {
-  member: string[];
-  days: number;
-  started: boolean;
+//any Type - 모든 자료형 허용해줌 => 타입실드 해제문법임. 타입스크립트 쓰는 의미가 없음. 기능 잃음.
+//그래서 타입관련 버그가 나도 잡아주지 않음
+let 이름: any;
+이름 = 123;
+이름 = [];
+
+//unknown 타입 - 모든 자료형 허용해줌. 최신문법. any문제해결
+let 네임: unknown;
+네임 = 123;
+네임 = {};
+//네임 - 1; // unknown타입은 number타입이 아니라 수학연산이 안됨. 같은 타입끼리의 연산만 가능함. 엄격함.
+
+//let 변수1: string = 네임; // unknown시에는 오류남, any는 오류안남
+
+let 나이: string | number;
+//나이 + 1; //안됨. 유니언타입에서는 안됨. 둘중 하나의 타입이었으면 +연산가능함.
+
+//문제1
+let user: string = "kim";
+let age: number | undefined = undefined;
+let married: boolean = false;
+let 철수: (string | number | boolean)[] = [user, age, married];
+
+//문제2
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
 } = {
-  member: ["kim", "park"],
-  days: 30,
-  started: true,
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
 };
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
